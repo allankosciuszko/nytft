19251b641784d819356741839be819c97c387f54
allankosciuszko <bartosz.ratajczyk@gmail.com>
2020-04-02 01:46:30 +0200

    Adds tests for most shared articles

---
+++ test/test/test_most_shared_article.py
@@ -0,0 +1,51 @@
+from __future__ import absolute_import
+
+import os
+import sys
+import unittest
+
+import swagger_client
+from swagger_client.rest import ApiException
+
+
+class TestMostSharedArticle(unittest.TestCase):
+
+    def setUp(self):
+        apiKey = os.getenv('NYFT_API_KEY')
+        self.assertNotIn(apiKey, [None, ""])
+        swagger_client.configuration.api_key = {"api-key": apiKey}
+        self.api = swagger_client.apis.most_popular_api.MostPopularApi()
+
+    def tearDown(self):
+        pass
+
+    def test_period_can_be_only_1_7_30_days(self):
+        periods = [1, 7, 30]
+        for period in periods:
+            with self.subTest():
+                response = self.api.viewed_period_json_get(period=period)
+                self.assertEqual(len(response.results), 20)
+
+        periods = [-30, -7, -1, 0, 1, 8, 40]
+        for period in periods:
+            with self.subTest():
+                try:
+                    self.api.viewed_period_json_get(period=period)
+                except ApiException as e:
+                    self.assertEqual(e.status, 404)
+
+    def test_response_has_20_articles(self):
+        response = self.api.viewed_period_json_get(period=1)
+        self.assertEqual(len(response.results), 20)
+
+    def test_you_can_specify_just_facebook_share_type(self):
+        response = self.api.viewed_period_json_get(period=7, share_type="facebook")
+        self.assertEqual(len(response.results), 20)
+        try:
+            self.api.viewed_period_json_get(period=7, share_type="twitter")
+        except  ApiException as e:
+            self.assertEqual(e.status, 404)
+
+
+if __name__ == '__main__':
+    unittest.main()
